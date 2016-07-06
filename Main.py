2c2f0814ec857e2e5ca5e085284f40e98b2a7611
AstureS <romart5@yandex.ru>
2016-07-06 17:03:46 +0300

    Диалог спрашивающий восстановить ли результат если такой есть. Если у пользователя отсутствует уже сделаный прогноз, то сразу предлагает создать его

--- Main.py
+++ Main.py
@@ -1,13 +1,20 @@
 import Entry
 import BioMenu
+import DBAccess
 
-reg = Entry.AskIsRegistered()
+reg = Entry.AskIsRegistered() #
 
 if reg:
-	user = Entry.AskLoginPass()			
+	user = Entry.AskLoginPass() #			
 else: 
-	user = Entry.Register()
+	user = Entry.Register() #
 
-print("Welcome, %s\n" % user)
+print("Welcome, %s\n" % user) #
 
-BioMenu.AskBiorhythms(user)
+userData = DBAccess.GetDateOfBirth(user) #
+recover = BioMenu.AskIsRecoverResults(userData)
+
+if recover:
+	print("Recover data")
+else:
+	BioMenu.AskBiorhythms(userData) #
