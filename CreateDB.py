4003e1e5b31f39c8c8cf7f7c2630157de676811a
AstureS <romart5@yandex.ru>
2016-07-06 13:14:52 +0300

    Обновлена структура БД. Добавлена таблица с запросами, где хранится когда и на сколько пользователь делал прогноз

--- CreateDB.py
+++ CreateDB.py
@@ -4,11 +4,10 @@ connector = sql.connect('BioDataBase.db')
 cursor = connector.cursor()
 cursor.execute("PRAGMA foreign_keys = ON")
 cursor.execute('CREATE TABLE users (id INTEGER PRIMARY KEY, login TEXT, password TEXT, date_of_birth TEXT)')
-cursor.execute('CREATE TABLE data (user_id INTEGER, date TEXT, physical REAL, emotional REAL, intellectual REAL, FOREIGN KEY(user_id) REFERENCES users(id))')
+cursor.execute('CREATE TABLE data (user_id INTEGER, request_id INTEGER, date TEXT, physical REAL, emotional REAL, intellectual REAL, FOREIGN KEY(user_id) REFERENCES users(id), FOREIGN KEY(request_id) REFERENCES request(id))')
+cursor.execute('CREATE TABLE request (id INTEGER PRIMARY KEY, user_id INTEGER, date TEXT, duration INTEGER, FOREIGN KEY(user_id) REFERENCES users(id))')
 
-cursor.execute('INSERT INTO users (login, password, date_of_birth) VALUES ("Admin", "Admin", "1995-10-05")')
-cursor.execute('INSERT INTO users (login, password, date_of_birth) VALUES ("Olga", "Qwerty", "1996-10-24")')
+cursor.execute('INSERT INTO users (login, password, date_of_birth) VALUES ("admin", "admin", "24-10-1996")')
 
 connector.commit()
-
 connector.close()
