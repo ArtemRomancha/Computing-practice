bc807bc1b0b6941f50b1f9f4869f6bd7b4d21562
AstureS <romart5@yandex.ru>
2016-07-08 01:07:37 +0300

    Добавлено меню восстановления предыдущего результата

--- BioMenu.py
+++ BioMenu.py
@@ -17,7 +17,7 @@ def AskBiorhythms(user):
 			print("Неверная продолжительность. Попробуйте снова")
 
 def AskIsRecoverResults(user):
-	if DBAccess.HaveResults(user[0]):
+	if DBAccess.HaveResults(user):
 		result = input("Хотите восстановить предыдущий результат? (Y/N)\n")
 		while 1: 		
 			result = result.lower()
@@ -31,4 +31,48 @@ def AskIsRecoverResults(user):
 	else:
 		return False
 		
+def AskRecoverVersion(user):
+	print("Укажите интервал прогноза")	
+	versions = DBAccess.GetRequest(user)
+	for i in range(0, len(versions)):
+		date = datetime.strptime(versions[i][2], '%d-%m-%Y')
+		d = timedelta(days = int(versions[i][3]))
+		print("%i) %10s - %10s" % (i+1, date.strftime('%d-%m-%Y'), (date + d).strftime('%d-%m-%Y')))
+	
+	while 1:
+		answer = int(input())
+		if answer - 1 < len(versions):
+			return versions[answer - 1]
+		else:
+			print("Неверный индекс")
+		
+def RecoverVersion(request):
+	d = timedelta(days = int(request[3]))	
+	print("Восстанавливаем данные в промежутке %s - %s" % (request[2], (datetime.strptime(request[2], '%d-%m-%Y') + d).strftime('%d-%m-%Y')))
+	data = DBAccess.GetData(request[0])
+	
+	DataSet = []
+	
+	for line in data:  		
+		userID = int(line[0])
+		requestID = int(line[1])
+		date = line[2]
+		phis = float(line [3])
+		emo = float(line [4])
+		intell = float(line[5])
+		DataSet.append([userID, requestID, date, phis, emo, intell])
+		
+	#Тут вызывать построение графика
 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
