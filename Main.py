20e4c6fafb2d00f12425f93c846a6729b37fdb3f
AstureS <romart5@yandex.ru>
2016-07-08 02:03:53 +0300

    Запрос восстановить несколько результатов

--- Main.py
+++ Main.py
@@ -15,7 +15,14 @@ userData = DBAccess.GetDateOfBirth(user) #
 recover = BioMenu.AskIsRecoverResults(userData[0])
 
 if recover:
-	res = BioMenu.AskRecoverVersion(userData[0])
-	BioMenu.RecoverVersion(res)
+	while 1:
+		res = BioMenu.AskRecoverVersion(userData[0])
+		BioMenu.RecoverVersion(res)
+		if not BioMenu.AskIsRecoverResults(userData[0]):
+			break
+	if BioMenu.AskBuildNew():
+		BioMenu.AskBiorhythms(userData) 
 else:
 	BioMenu.AskBiorhythms(userData) #
+
+print("До свидания, %s" % user)
