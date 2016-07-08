3cc15ab6509004bc65a66154635d82a23da295d5
Stefany Amam <stefa19942011@gmail.com>
2016-07-08 12:52:02 +0300

    График изменен

--- Graph.py
+++ Graph.py
@@ -1,5 +1,3 @@
-# ­*­ coding: utf­8 ­*­
-#import Simulation
 import tkinter
 import math
 #
@@ -12,29 +10,29 @@ def CreateGraph(DataSet,duration):
     canvas["background"]="#eeeeff"
     canvas["borderwidth"]=2
     canvas.pack()
-    # рисуем 1-ый график
+    # СЂРёСЃСѓРµРј 1-С‹Р№ РіСЂР°С„РёРє
     points1=[]
     ay=300
     y0=300
     x0=40
-    x1=x0+45*duration
-    dx=45
+    x1=x0+40*duration
+    dx=40
     j=0
-    # цикл для определения всех точек 1-ого графика
+    # С†РёРєР» РґР»СЏ РѕРїСЂРµРґРµР»РµРЅРёСЏ РІСЃРµС… С‚РѕС‡РµРє 1-РѕРіРѕ РіСЂР°С„РёРєР°
     for n in range(x0,x1,dx):
         if DataSet[j][3]>=0:
             pp=(n,310-3*DataSet[j][3])
         else:
             pp=(n,-3*DataSet[j][3]+310)
-        canvas.create_text(x0+45*j,640,text=DataSet[j][2],font= "Arial 6") #подписываем даты на графике DataSet[j][2]
+        canvas.create_text(x0+40*j,640,text=DataSet[j][2],font= "Arial 6") #РїРѕРґРїРёСЃС‹РІР°РµРј РґР°С‚С‹ РЅР° РіСЂР°С„РёРєРµ DataSet[j][2]
         j=j+1
         points1.append(pp)
-        #выводим график
+        #РІС‹РІРѕРґРёРј РіСЂР°С„РёРє
     canvas.create_line(points1,fill="blue",smooth=1)
-    # рисуем 2-ой график
+    # СЂРёСЃСѓРµРј 2-РѕР№ РіСЂР°С„РёРє
     j=0
     points2=[]
-    # цикл для определения всех точек 2-ого графика
+    # С†РёРєР» РґР»СЏ РѕРїСЂРµРґРµР»РµРЅРёСЏ РІСЃРµС… С‚РѕС‡РµРє 2-РѕРіРѕ РіСЂР°С„РёРєР°
     for n in range(x0,x1,dx):
         if DataSet[j][4]>=0:
             pp=(n,310-3*DataSet[j][4])
@@ -42,12 +40,12 @@ def CreateGraph(DataSet,duration):
             pp=(n,-3*DataSet[j][4]+310)
         j=j+1
         points2.append(pp)
-    # выводим график
+    # РІС‹РІРѕРґРёРј РіСЂР°С„РёРє
     canvas.create_line(points2,fill="green",smooth=1)
-    # рисуем 3-ий график
+    # СЂРёСЃСѓРµРј 3-РёР№ РіСЂР°С„РёРє
     j=0
     points3=[]
-    # цикл для определения всех точек 2-ого графика
+    # С†РёРєР» РґР»СЏ РѕРїСЂРµРґРµР»РµРЅРёСЏ РІСЃРµС… С‚РѕС‡РµРє 2-РѕРіРѕ РіСЂР°С„РёРєР°
     for n in range(x0,x1,dx):
         if DataSet[j][5]>=0:
             pp=(n,310-3*DataSet[j][5])
@@ -55,9 +53,9 @@ def CreateGraph(DataSet,duration):
             pp=(n,-3*DataSet[j][5]+310)
         j=j+1
         points3.append(pp)
-    # выводим график
+    # РІС‹РІРѕРґРёРј РіСЂР°С„РёРє
     canvas.create_line(points3,fill="red",smooth=1)
-    # рисуем координатные оси
+    # СЂРёСЃСѓРµРј РєРѕРѕСЂРґРёРЅР°С‚РЅС‹Рµ РѕСЃРё
     y_axe=[]
     yy=(x0,0)
     y_axe.append(yy)
@@ -88,8 +86,8 @@ def CreateGraph(DataSet,duration):
             j=j+1
     canvas.create_text(25,610,fill="black",text=-100)
     #
-    canvas.create_text(300,670,fill="blue",text="- физическое состояние")
-    canvas.create_text(300,680,fill="green",text="- эмоциональное состояние")
-    canvas.create_text(300,690,fill="red",text="- интеллектуальное состояние")
+    canvas.create_text(300,670,fill="blue",text="- С„РёР·РёС‡РµСЃРєРѕРµ СЃРѕСЃС‚РѕСЏРЅРёРµ")
+    canvas.create_text(300,680,fill="green",text="- СЌРјРѕС†РёРѕРЅР°Р»СЊРЅРѕРµ СЃРѕСЃС‚РѕСЏРЅРёРµ")
+    canvas.create_text(300,690,fill="red",text="- РёРЅС‚РµР»Р»РµРєС‚СѓР°Р»СЊРЅРѕРµ СЃРѕСЃС‚РѕСЏРЅРёРµ")
     #
     tk.mainloop()
