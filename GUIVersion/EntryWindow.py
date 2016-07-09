cda853c4d4e6e10e96d6967a88440f3d1cf0bc6f
AstureS <romart5@yandex.ru>
2016-07-09 23:09:35 +0300

    Окно входа в систему

---
+++ GUIVersion/EntryWindow.py
@@ -0,0 +1,61 @@
+from tkinter import *
+import tkinter.messagebox as tm
+import DBAccess as DB
+
+class LoginFrame(Frame):
+	def __init__(self, master):
+		super().__init__(master)
+		
+		self.userLB = Label(self, text="Username")
+		self.passLB = Label(self, text="Password")
+		
+		self.userEntry = Entry(self)
+		self.passEntry = Entry(self, show="*")
+		
+		self.userLB.grid(row=0, sticky=E)
+		self.passLB.grid(row=1, sticky=E)
+		
+		self.userEntry.grid(row=0, column=1)
+		self.passEntry.grid(row=1, column=1)
+				
+		self.logbtn = Button(self, text="Login", command = self._login_btn_clickked)
+		self.logbtn.grid(columnspan = 2)
+		
+		self.regbtn = Button(self, text="Registration", command = self._reg_btn_clickked)
+		self.regbtn.grid(columnspan = 2)
+		
+		self.pack()
+	def _reg_btn_clickked(self):
+		print("Registration")
+		#Call registration modul
+
+	def _login_btn_clickked(self):
+        #print("Clicked")
+		username = self.userEntry.get()
+		password = self.passEntry.get()
+
+        #print(username, password)
+
+		if DB.Control(username, password):
+			tm.showinfo("Login info", "Welcome, %s" % username)
+		else:
+			tm.showerror("Login error", "Incorrect username or password")
+			#self.master.destroy()			
+
+def set_center(root):
+	root.withdraw()
+	root.update_idletasks()  # Update "requested size" from geometry manager
+	x = (root.winfo_screenwidth() - root.winfo_reqwidth()) / 2
+	y = (root.winfo_screenheight() - root.winfo_reqheight()) / 2
+	root.geometry("+%d+%d" % (x, y))
+	root.deiconify()
+
+
+def Run():
+	root = Tk()
+	root.title("Login")
+	lf = LoginFrame(root)
+	set_center(root)
+	root.mainloop()
+
+
